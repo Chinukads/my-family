b15b5af49cdcb63802d53f7c5944c1fe95120ce0
Chinukads <72682150+Chinukads@users.noreply.github.com>
2020-12-17 15:02:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+
+   var walk= [
+    "father-clipart-10.jpg",
+    "OIP.jpg"
+
+  ]
+  var i=0;
+  var left1 = 100;
+  
+   
+  
+  function walker(){
+    
+  document.getElementById("img1").style.position="relative"; document.getElementById("img1").style.left= left1+10+"px";
+  document.getElementById("img1").src= walk[i] ;
+    i++;
+  
+  if (i==2) {
+    i=0;
+  }
+"
+  ]
+  var i=0;
+  var left1 = 100;
+  
+   
+  
+  function walker(){
+    
+  document.getElementById("img1").style.position="relative"; document.getElementById("img1").style.left= left1+10+"px";
+  document.getElementById("img1").src= walk[i] ;
+    i++;
+  
+  if (i==2) {
+    i=0;
+  }
+  }
